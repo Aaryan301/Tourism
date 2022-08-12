17bb534d27000aebe0f6d6b3887cac4b8736aaf2
Aaryan301 <86154156+Aaryan301@users.noreply.github.com>
2022-08-12 21:55:12 +0530

    files upload

---
+++ script.js
@@ -0,0 +1,40 @@
+/*let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+.to('.rock1',3,{y:-600})
+.to('.girl1',2,{y:-500},'-=3')
+.to(".bg1",2,{y: -700},"-=3")
+.to('.content',3,{top: "0%"}, '-=3');
+
+
+let scene= new ScrollMagic.Scene(
+{
+    triggerElement: "section",
+        triggerHook:"onLeave",
+    duration:"400%",
+
+})
+
+.setTween(timeline)
+.addTo(controller);
+
+
+let controller2= new ScrollMagic.Controller();
+let timeline2 = new TimelineMax();
+
+timeline2
+.from('.contentimages',1,{y:-100})
+.from('.contentimages',3,{opacity: -2});
+
+let scene2= new ScrollMagic.Scene(
+{
+    triggerElement: "swizsection",
+        triggerHook:"onLeave",
+    duration:"350%",
+
+})
+
+.setTween(timeline2)
+.addTo(controller2);
+*/
